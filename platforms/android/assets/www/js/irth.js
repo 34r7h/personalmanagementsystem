badf4397a2f0e218a26b6e9c325475245cad7628
irthos <irth03@gmail.com>
2015-10-13 13:59:33 -0700

    notes fix for mobile and extension

--- platforms/android/assets/www/js/irth.js
+++ platforms/android/assets/www/js/irth.js
@@ -4,18 +4,40 @@ angular.module('irth', ['firebase', 'mm.foundation'])
 		var dbURL = 'https://yourlife.firebaseio.com/users',
 			ref = {}, backgroundsRef = {}, sync = {}, backgroundsSync = {}, bind = {}, authRef = new Firebase(dbURL);
 		$scope.lifestyle = ['action', 'event', 'fuel', 'train', 'day', 'task', 'note', 'fear', 'love'];
+
+		$scope.lifestyleDescription = {
+			action: 'These are things you do',
+			event: 'These are things that happen',
+			fuel: 'Fuels are anything that you feed yourself',
+			train: 'Training is exercise and/or education',
+			day: 'Treat the day section as your journal',
+			task: 'Tasks are created and updated here',
+			fear: 'Express your negative feelings and emotions',
+			love: 'Give thanks and praise. You\'re really quite lucky :)'
+		};
+
 		$scope.nav = {ALPHA: ['action', 'task'], BETA: ['fuel', 'train'], PHI: ['day', 'event'], OMEGA: ['fear', 'love']};
 		$scope.style = {};
+		$scope.backgroundsImg = 'images/tracks.jpg'
+		$scope.dimensions = {
+			minWidth: $window.innerWidth + 'px',
+			minHeight: $window.innerHeight + 'px',
+			background: 'url(' + $scope.backgroundsImg + ') center center no-repeat fixed',
+			backgroundSize: 'cover'
+	};
+
 		$scope.style.note = {
 			position: 'fixed',
 			bottom: 0,
 			left: 0,
-			width: $window.innerWidth + 'px',
-			height: ( $window.innerHeight / 2 ) + 'px',
+			minWidth: $window.innerWidth + 'px',
+			minHeight: ( $window.innerHeight / 2 ) + 'px',
+			width: '100%',
+			height: '50%',
 			zIndex: 10000,
-			background: 'rgba(23,43,12, .62)'
+			background: 'rgba(23,43,12, .62)',
+			display:'flex'
 		};
-		$scope.dimensions = {minWidth: $window.innerWidth + 'px', minHeight: $window.innerHeight + 'px'};
 
 
 		$scope.show = {note: {big: true}};
